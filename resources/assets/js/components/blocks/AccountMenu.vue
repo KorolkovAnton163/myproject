560394e6a825a432bc938f4fbf74d6f75d9940c3
Антон Корольков <a.korolkov@river-city.ru>
2017-04-11 18:54:46 +0400

    Fix account menu

--- resources/assets/js/components/blocks/AccountMenu.vue
+++ resources/assets/js/components/blocks/AccountMenu.vue
@@ -1,15 +1,38 @@
-<template>
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
     <div class="account-menu">
-        <router-link :to="{ name:'profile' }">Profile</router-link>
-        <router-link :to="{ name:'bookmarks' }">Bookmarks</router-link>
-        <router-link v-if="user && user.canRolesEdit" :to="{ name:'role-edit' }">Profile</router-link>
+        <div class="menu-button htx ripple-effect" v-on:click="showMenu"
+             v-bind:class="{ 'is-active': show }">
+            <span>opened</span>
+        </div>
+        <div class="menu-wrapper" v-show="show">
+            <router-link :to="{ name:'profile' }">Profile</router-link>
+            <router-link :to="{ name:'bookmarks' }">Bookmarks</router-link>
+            <router-link v-if="user && user.canRolesEdit" :to="{ name:'role-edit' }">Profile</router-link>
+        </div>
     </div>
 </template>
 <script>
     module.exports = {
+        data () {
+            return {
+                show: window.innerWidth > 650
+            }
+        },
         computed: {
             user () {
                 return this.$store.getters.user
+            }
+        },
+        mounted() {
+            window.addEventListener('resize', () => {
+                this.show = window.innerWidth > 650;
+            });
+        },
+        methods: {
+            showMenu () {
+                if (window.innerWidth < 650) {
+                    this.show = !this.show;
+                }
             }
         }
     }
