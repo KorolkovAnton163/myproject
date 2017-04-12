f090dbba120ae519a7c35d4b46a50ffb21f3b4ba
Антон Корольков <a.korolkov@river-city.ru>
2017-04-12 18:26:30 +0400

    fix show button

--- resources/assets/js/components/blocks/AccountMenu.vue
+++ resources/assets/js/components/blocks/AccountMenu.vue
@@ -2,7 +2,7 @@
     <div class="account-menu">
         <div class="menu-wrapper" v-if="user">
             <router-link :to="{ name:'profile' }">Profile</router-link>
-            <router-link :to="{ name:'bookmarks' }">Bookmarks</router-link>
+            <router-link class="bookmark" :to="{ name:'bookmarks' }">Bookmarks</router-link>
             <router-link v-if="user && user.canRolesEdit" :to="{ name:'role-edit' }">Roles</router-link>
         </div>
     </div>
