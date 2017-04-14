f84ca9a9e023cff2b720d1be04155c98865cd539
Антон Корольков <a.korolkov@river-city.ru>
2017-04-14 15:46:55 +0400

    Fix

--- resources/assets/js/components/blocks/PostDescription.vue
+++ resources/assets/js/components/blocks/PostDescription.vue
@@ -13,7 +13,7 @@
                 <p><strong>Description:</strong> {{ post.description }}</p>
             </div>
             <div class="action-block">
-                <bookmark :post="post" v-if="post && user"></bookmark>
+                <bookmark :post="post" v-if="user"></bookmark>
             </div>
         </div>
     </div>
